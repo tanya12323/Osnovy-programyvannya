18cabce2562c498a77e81462bd75ad6c2d5440cb
Yakymchuk Tetiana <hanaknaed.nessa@gmail.com>
2026-09-20 02:46:51 +0300

    Create lab1.py

---
+++ lab1.py
@@ -0,0 +1,33 @@
+print ("Hello World")
+
+my_int = 3
+my_float = 3.14
+my_string = "abc"
+my_bool = True
+my_list = [1, 2, 3]
+my_tuple = (1, 2, 3)
+my_dict = {1: "One", 2: "Two", 3: "Three"}
+my_set = set(my_list)
+
+print("")
+print("int =", my_int)
+print("float =", my_float)
+print("string =", my_string)
+print("bool =", my_bool)
+print("list =", my_list)
+print("tuple =", my_tuple)
+print("dict =", my_dict)
+print("set =", my_set)
+
+print("")
+x = 10
+y = 3
+print("x + y =", x + y)
+print("x - y =", x - y)
+print("x * y =", x * y)
+print("x / y =", x / y)
+print("x // y =", x // y)
+print("x % y =", x % y)
+print("x ** y =", x ** y)
+print("x == y:", x == y)
+"""це є коментар"""
